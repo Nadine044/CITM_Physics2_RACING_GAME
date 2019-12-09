6aa9c544ad72cb1d1ff73fee2f9c850c83347917
Nadine044 <36292059+Nadine044@users.noreply.github.com>
2019-12-09 16:16:46 +0100

    Adding Vehicle Placeholder

---
+++ Racing_Game/PhysVehicle3D.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "PhysBody3D.h"
+#include "glmath.h"
+
+class btRaycastVehicle;
+struct PhysBody3D;
+
+struct Wheel
+{
+	vec3 connection; // origin of the ray. Must come from within the chassis
+	vec3 direction; 
+	vec3 axis;
+	float suspensionRestLength; // max length for suspension in meters
+	float radius;
+	float width;
+	bool front; // is front wheel ?
+	bool drive; // does this wheel received engine power ?
+	bool brake; // does breakes affect this wheel ?
+	bool steering; // does this wheel turns ?
+};
+
+struct VehicleInfo
+{
+	//Golf
+	vec3 floor;
+	vec3 floor_offset;
+
+
+	vec3 cube1;
+	vec3 cube1_offset;
+	vec3 cube2;
+	vec3 cube2_offset;
+	vec3 cube3;
+	vec3 cube3_offset;
+	vec3 cube4;
+	vec3 cube4_offset;
+	vec3 cube5;
+	vec3 cube5_offset;
+	vec3 window;
+	vec3 window_offset;
+	vec3 cilindre1;
+	vec3 cilindre1_offset;
+	vec3 cylindre2;
+	vec3 cylindre2_offset;
+	vec3 cylindre3;
+	vec3 cylindre3_offset;
+
+	~VehicleInfo();
+	
+	vec3 chassis_size;
+	vec3 chassis_offset;
+	float mass;
+	float suspensionStiffness; // default to 5.88 / 10.0 offroad / 50.0 sports car / 200.0 F1 car
+	float suspensionCompression; // default to 0.83
+	float suspensionDamping; // default to 0.88 / 0..1 0 bounces / 1 rigid / recommended 0.1...0.3
+	float maxSuspensionTravelCm; // default to 500 cm suspension can be compressed
+	float frictionSlip; // defaults to 10.5 / friction with the ground. 0.8 should be good but high values feels better (kart 1000.0)
+	float maxSuspensionForce; // defaults to 6000 / max force to the chassis
+
+	Wheel* wheels;
+	int num_wheels;
+};
+
+
+struct PhysVehicle3D : public PhysBody3D
+{
+public:
+	PhysVehicle3D(btRigidBody* body, btRaycastVehicle* vehicle, const VehicleInfo& info);
+	~PhysVehicle3D();
+
+	void Render();
+	void ApplyEngineForce(float force);
+	void Brake(float force);
+	void Turn(float degrees);
+	float GetKmh() const;
+public:
+
+	VehicleInfo info;
+	btRaycastVehicle* vehicle;
+};
